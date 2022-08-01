13465c8e602b8581ee86d8a260b9c4e2d1fd1319
Hendry Wiranto <hendrywiranto24@gmail.com>
2022-08-02 01:12:25 +0700

    62. Unique Paths (#62)
    62

---
+++ medium/62. Unique Paths/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m, vector<int> (n, 0));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 || j == 0) grid[i][j] = 1;
+                else grid[i][j] = grid[i][j-1] + grid[i-1][j];
+            }
+        }
+
+        return grid[m-1][n-1];
+    }
+};
