e3244350e2020570b9f522e339ee8e34838ca1ee
hendrywiranto <hendrywiranto24@gmail.com>
2023-01-23 23:33:23 +0700

    add solution to 997

--- easy/997. Find the Town Judge/main.cpp
+++ easy/997. Find the Town Judge/main.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>> matrix(n+1, vector<int>(n+1, 0));
+        vector<int> trustLevel(n, 0);
+
         for(auto tr:trust){
-            matrix[tr[0]][tr[1]] = 1;
+            trustLevel[tr[1]-1]++;
+            trustLevel[tr[0]-1]--;
+        }
+        for(int i=0;i<trustLevel.size();i++){
+            if(trustLevel[i] == n-1) return i+1;
         }
 
-        for(int i=1;i<=n;i++){
-            bool candidate = true;
-            for(int j=1;j<=n;j++){
-                if(j == i) continue;
-                if(matrix[j][i] == 0){
-                    candidate = false;
-                    break;
-                }
-            }
-            if(candidate){
-                bool found = true;
-                for(int j=1;j<=n;j++){
-                    if(matrix[i][j] == 1){
-                        found = false;
-                        break;
-                    }
-                }
-                if(found) return i;
-            }
-        }
         return -1;
     }
 };
--- easy/997. Find the Town Judge/remarks.md
+++ easy/997. Find the Town Judge/remarks.md
@@ -2,10 +2,10 @@
 https://leetcode.com/problems/find-the-town-judge/description/
 
 # Link to submitted discussion
-https://leetcode.com/problems/find-the-town-judge/solutions/3090931/c-using-adjacency-matrix/
+https://leetcode.com/problems/find-the-town-judge/solutions/3091002/c-o-n-time-and-space/
 
 # Time Complexity
-`O(n^2)`
+`O(n)`
 
 # Memory Complexity
-`O(n^2)`
+`O(n)`
